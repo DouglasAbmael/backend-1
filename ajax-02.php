6d6adbbbb6703e3cc2d4b7399bf27ccc1406191a
aluno <brunacscamilotto@gmail.com>
2024-05-02 16:22:30 -0300

    aqui
    commit

---
+++ ajax-02.php
@@ -0,0 +1,35 @@
+<?php
+$filename = "txt/clientes.csv";
+
+//tenta abrir o arquivo
+$file = fopen($filename, "r");
+
+// verifica se o arquivo foi aberto
+if ($file) {
+   // criar array "clientes" vazio
+   $aClientes = [];
+   //cria cabeçalho vazio
+   $header = null;
+
+   // processa as linhas do arquivo
+   while (($data = fgetcsv($file,1000,";")) !== FALSE) {
+      //cria o header a partir da primeira linha
+      if($header==null){
+         $header = $data;
+         continue;
+      }
+      //cria um arrey associativo
+      $newdata = [];
+      for ($i=0; $i<sizeof($data); $i++){
+      $newdata[$header[$i]]=$data[$i];
+      }
+      // adicionar $data ao array "clientes;
+      array_push($aClientes, $data);
+   }
+   // converter array em json
+   $json = json_encode($aClientes);
+   // printar o json
+   print($json);
+   fclose($file);
+}
+?>
